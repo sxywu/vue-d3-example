9d0581e29fda6cef0aa023f9b0c69c83a9d9c36c
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-11-11 23:48:38 +0100

    Draw histogram

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
+    <!-- HEADER -->
     <h1>The Seasonality of Box Office Hits</h1>
     <p class='description'>
-In the last decade, most block buster hits have happened around or during the summer and winter holidays.  <strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and <strong>hover</strong> the movies to see more details.
+In the last decade, most block buster hits have happened around or during the summer and winter holidays.
+<strong>Brush</strong> the histograms to filter the movies by metascore and/or box office figures, and <strong>hover</strong> the movies to see more details.
     </p>
+    <!-- HISTOGRAM -->
+    <Histogram v-for='({id, label, format}) in histograms'
+      v-bind='{movies, filtered, id, label, format}' />
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
 import * as d3 from 'd3'
+import Histogram from './components/Histogram'
 
 const startYear = 2008;
 const numYears = 10;
@@ -17,10 +23,16 @@ const numYears = 10;
 export default {
   name: 'app',
   components: {
+    Histogram,
   },
   data() {
     return {
       movies: [],
+      filtered: [],
+      histograms: [
+        {id: 'score', label: 'metascores'},
+        {id: 'boxOffice', label: 'box office figures', format: d => `$${parseInt(d/ 1000000)}M`},
+      ],
     }
   },
   mounted() {
@@ -31,6 +43,7 @@ export default {
           .map(d => Object.assign(d, {date: new Date(d.date)}))
           .filter(d => d.boxOffice && d.year >= startYear)
           .value();
+        this.filtered = this.movies
       });
   }
 }
---
+++ src/components/Histogram.vue
@@ -0,0 +1,96 @@
+<template>
+  <svg class='histogram' :width='width' :height='height'>
+    <g class='bars'>
+      <rect v-for='(d, i) in bars' :key='i' :x='d.x' :width='d.width'
+        :y='d.y' :height='d.height' :fill=d.fill :stroke='d.fill' />
+    </g>
+    <g ref='xAxis' :transform='`translate(0, ${height - margin.bottom})`' />
+    <g ref='brush' />
+  </svg>
+</template>
+
+<script>
+import _ from 'lodash'
+import * as d3 from 'd3'
+
+const width = 500
+const height = 120
+const margin = {top: 20, right: 20, bottom: 20, left: 20}
+
+export default {
+  name: 'histogram',
+  props: ['movies', 'filtered', 'id', 'label', 'format'],
+  data() {
+    return {
+      width, height, margin,
+      bars: [],
+    }
+  },
+  created() {
+    this.xScale = d3.scaleLinear().range([margin.left, width - margin.right])
+    this.histogram = d3.histogram()
+
+    this.yScale = d3.scaleLinear().range([height - margin.bottom, margin.top])
+    this.colorScale = d3.scaleSequential(d3.interpolateViridis)
+
+    this.xAxis = d3.axisBottom().scale(this.xScale).tickFormat(this.format)
+  },
+  watch: {
+    movies: function() {
+      this.calculateScales()
+      d3.select(this.$refs.xAxis).call(this.xAxis)
+    },
+    filtered: function() {
+      this.calculateData()
+    }
+  },
+  methods: {
+    calculateScales: function() {
+      if (!this.movies.length) return
+
+      // colors
+      const colorDomain = d3.extent(this.movies, d => d.score);
+      this.colorScale.domain(colorDomain).nice()
+
+      // x is the attribute
+      const xDomain = d3.extent(this.movies, d => d[this.id])
+      this.xScale.domain(xDomain).nice()
+
+      // get the bins
+      this.histogram.domain(this.xScale.domain())
+        .thresholds(this.xScale.ticks(50))
+        .value(d => d[this.id])
+      const bins = this.histogram(this.movies)
+
+      // calculate y from the bins
+      const yMax = d3.max(bins, d => d.length)
+      this.yScale.domain([0, yMax])
+    },
+    calculateData: function() {
+      if (!this.filtered.length) return
+
+      // use filtered for bins now that scales are calculated
+      const bins = this.histogram(this.filtered)
+
+      // calculate rect bar for each bin
+      this.bars = bins.map(d => {
+        const {x0, x1} = d
+        const x = this.xScale(x0)
+        const y = this.yScale(d.length)
+        const median = d3.median(d, d => d.score) || 0
+
+        return {
+          x,
+          width: this.xScale(x1) - x,
+          y,
+          height: height - margin.bottom - y,
+          fill: this.colorScale(median),
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
